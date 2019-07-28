13d50fe1f012f5bd53797e86de2799db0839362a
Marios Ntoulas <ntoulasm@csd.uoc.gr>
2019-07-28 10:44:20 +0300

    add .vscode folder

--- isense/extension.js
+++ isense/extension.js
@@ -9,9 +9,6 @@ function activate(context) {
 
 	console.log('isense activated');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.isense', function () {
 		vscode.window.showInformationMessage('Extension isense says Hello World!');
 	});
