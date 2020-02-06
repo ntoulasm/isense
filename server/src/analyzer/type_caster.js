7695f1976d8ac00e0d574036ff5196dc940c8c86
Marios Ntoulas <csd3260@csd.uoc.gr>
2020-02-06 13:05:20 +0200

    Implement type cast functions

--- server/src/analyzer/type_caster.js
+++ server/src/analyzer/type_caster.js
@@ -3,6 +3,47 @@ const Utility = require('../utility/utility');
 
 const TypeCaster = {};
 
+TypeCarrier.toNumber = type => {
+
+    const newType = {};
+    newType.id = TypeCarrier.Type.Number;
+
+    switch(type.id) {
+        case TypeCarrier.Type.Number: {
+            if(type.hasOwnProperty("value")) {
+                newType.value = type.value;
+            }
+            break;
+        }
+        case TypeCarrier.Type.String:
+        case TypeCarrier.Type.Boolean: {
+            if(type.hasOwnProperty("value")) {
+                newType.value = Number(type.value);
+            }
+            break;
+        }
+        case TypeCarrier.Type.Array:
+        case TypeCarrier.Type.Object:
+        case TypeCarrier.Type.Function:
+        case TypeCarrier.Type.Class:
+        case TypeCarrier.Type.Undefined: {
+            newType.value = NaN;
+            break;
+        }
+        case TypeCarrier.Type.Null: {
+            newType.value = 0;
+            break;
+        }
+        default: {
+            Utility.assert(false, `Can not cast '${TypeCarrier.typeText[type.id]}' to number`);
+            break;
+        }
+    }
+
+    return newType;
+
+};
+
 TypeCaster.toString = type => {
 
     const newType = {};
@@ -54,6 +95,44 @@ TypeCaster.toString = type => {
 
 };
 
+TypeCaster.toBoolean = type => {
+
+    const newType = {};
+    newType.id = TypeCarrier.Type.Boolean;
+
+    switch(type.id) {
+        case TypeCarrier.Type.Number:
+        case TypeCarrier.Type.String: {
+            if(type.hasOwnProperty("value")) {
+                newType.value = Boolean(type.value);
+            }
+            break;
+        }
+        case TypeCarrier.Type.Boolean: {
+            if(type.hasOwnProperty("value")) {
+                newType.value = type.value;
+            }
+            break;
+        }
+        case TypeCarrier.Type.Array:
+        case TypeCarrier.Type.Object:
+        case TypeCarrier.Type.Function:
+        case TypeCarrier.Type.Class: {
+            newType.value = true;
+            break;
+        }
+        case TypeCarrier.Type.Null:
+        case TypeCarrier.Type.Undefined: {
+            newType.value = false;
+            break;
+        }
+        default: {
+            Utility.assert(false, `Can not cast '${TypeCarrier.typeText[type.id]}' to boolean`);
+            break;
+        }
+    }
+
+    return newType;
 
 };
 
